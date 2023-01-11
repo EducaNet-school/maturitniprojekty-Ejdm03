8bf53b327ff7e59d5decdec87665872058ea8338
Ejdm03 <80262908+Ejdm03@users.noreply.github.com>
2023-01-11 10:07:10 +0100

    FOrmular

---
+++ 2.hodina/KontolaFormulare.php
@@ -0,0 +1,35 @@
+<?php
+
+    if (isset($_POST['ok'])){
+        $jmeno = $_POST["jmeno"];
+        $adresa= $_POST["adresa"];
+
+        if (empty($jmeno) || empty($adresa)) {
+            echo "Jméno a Adresa musí být zadány<br>";
+        } else {
+            if (str_word_count($jmeno) < 2) {
+                echo "Slovo musí mít minimálne 2 slova<br>";
+            } elseif (strlen($adresa) < 10) {
+                echo "Adresa musí obsahovat minimalne 10 znaků<br>";
+            } else {
+
+                echo "Jméno: " . $jmeno . "<br>";
+                echo "Adresa: " . $adresa . "<br>";
+
+            }
+        }
+    }
+?>
+
+
+<h1>Formulář</h1>
+
+
+<form action="KontolaFormulare.php" method="post">
+    <label for="name">Jmeno a Příjmení:</label>
+    <input type="text" id="jmeno" name="jmeno"><br>
+    <label for="address">Adresa:</label>
+    <input type="text" id="adresa" name="adresa"><br>
+    <input type="submit" value="ok" name="ok">
+</form>
+
