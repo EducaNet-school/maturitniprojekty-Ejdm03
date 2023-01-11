bd82282d70a39ab0ae6832c513a46398f8a0547e
Ejdm03 <80262908+Ejdm03@users.noreply.github.com>
2023-01-11 14:04:54 +0100

    Funkce oprava

--- 2.hodina/KontolaFormulare.php
+++ 2.hodina/KontolaFormulare.php
@@ -1,27 +1,34 @@
 <?php
 
-    if (isset($_POST['ok'])){
-        $jmeno = $_POST["jmeno"];
-        $adresa= $_POST["adresa"];
 
-        if (empty($jmeno) || empty($adresa)) {
-            echo "Jméno a Adresa musí být zadány<br>";
+function Kontrola()
+{
+    $jmeno = $_POST["jmeno"];
+    $adresa = $_POST["adresa"];
+
+    if (empty($jmeno) || empty($adresa)) {
+        echo "Jméno a Adresa musí být zadány<br>";
+    } else {
+        if (str_word_count($jmeno) < 2) {
+            echo "Slovo musí mít minimálne 2 slova<br>";
+        } elseif (strlen($adresa) < 10) {
+            echo "Adresa musí obsahovat minimalne 10 znaků<br>";
         } else {
-            if (str_word_count($jmeno) < 2) {
-                echo "Slovo musí mít minimálne 2 slova<br>";
-            } elseif (strlen($adresa) < 10) {
-                echo "Adresa musí obsahovat minimalne 10 znaků<br>";
-            } else {
 
-                echo "Jméno: " . $jmeno . "<br>";
-                echo "Adresa: " . $adresa . "<br>";
+            echo "Jméno: " . $jmeno . "<br>";
+            echo "Adresa: " . $adresa . "<br>";
 
-            }
         }
     }
+}
+
+
+if (isset($_POST['ok'])){
+    Kontrola();
+}
+
+
 ?>
-
-
 <h1>Formulář</h1>
 
 
